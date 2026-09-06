38584fb3183e3fb8fa421ef30e98347066710635
VINEET CHAWLA <vineetchawla19@gmail.com>
2026-09-06 16:59:36 +0530

    refactor: make Sonora a standalone media service

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import cors from "@fastify/cors";
 import jwt from "@fastify/jwt";
 import Fastify from "fastify";
-import { HttpAudiobookshelfClient } from "./clients/audiobookshelf.js";
 import { getConfig } from "./config.js";
 import { healthRoutes } from "./routes/health.js";
 
@@ -11,13 +10,6 @@ const app = Fastify({ logger: true });
 await app.register(cors, { origin: config.SONORA_ALLOWED_ORIGIN });
 await app.register(jwt, { secret: config.SONORA_JWT_SECRET });
 
-const audiobookshelf = new HttpAudiobookshelfClient(
-  config.ABS_BASE_URL,
-  config.ABS_SERVICE_API_KEY
-);
-
-await app.register((scope) => healthRoutes(scope, audiobookshelf), {
-  prefix: "/v1"
-});
+await app.register(healthRoutes, { prefix: "/v1" });
 
 await app.listen({ port: config.SONORA_PORT, host: "0.0.0.0" });
